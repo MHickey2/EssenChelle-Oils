89af36ca58ad003299278d6c8d703df52ef9f6fe
Michelle Hickey <michellehickey2@yahoo.ie>
2023-01-11 19:42:22 +0000

    Completed home page header and css

--- essenchelle/settings.py
+++ essenchelle/settings.py
@@ -152,3 +152,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

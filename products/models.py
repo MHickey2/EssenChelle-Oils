dd2e74ca71c0c2fbcfe831b32b0f65ecde1f8af6
Michelle Hickey <michellehickey2@yahoo.ie>
2023-01-14 18:58:44 +0000

    customized admin

--- products/models.py
+++ products/models.py
@@ -5,6 +5,9 @@ class Category(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return self.name
 

ab8dba2b80751ac8b141b92a059a7327f708a739
Michelle Hickey <michellehickey2@yahoo.ie>
2023-01-16 16:17:58 +0000

    added env.py to the project for site protection

--- essenchelle/settings.py
+++ essenchelle/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g!*vb@h6n=j=+s&0t^7x7i7j$st-1fo!=tzh#i7%1$w=0qq_fg'  # noqa
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

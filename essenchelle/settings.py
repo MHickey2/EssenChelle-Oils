db177113b1be6e9ee063b118bef954f83e47b579
Michelle Hickey <michellehickey2@yahoo.ie>
2023-01-23 19:23:49 +0000

    Added toasts to includes and to templates

--- essenchelle/settings.py
+++ essenchelle/settings.py
@@ -82,6 +82,8 @@ TEMPLATES = [
     },
 ]
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 AUTHENTICATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
@@ -89,6 +91,7 @@ AUTHENTICATION_BACKENDS = (
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
 )
+
 
 SITE_ID = 1
 

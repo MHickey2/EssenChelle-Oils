dfeb222474ea1f1a325c80b52dd1ef07212418de
Michelle Hickey <michellehickey2@yahoo.ie>
2023-01-28 19:04:31 +0000

    added checkout views and template

--- essenchelle/settings.py
+++ essenchelle/settings.py
@@ -49,6 +49,9 @@ INSTALLED_APPS = [
     'story',
     'bag',
     'checkout',
+
+    # other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -62,6 +65,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'essenchelle.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -77,11 +82,17 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # noqa required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
+
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 

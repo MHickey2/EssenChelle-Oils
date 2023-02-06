e089e2d9e45f37f3f621804f78e57e7bdd360122
Michelle Hickey <michellehickey2@yahoo.ie>
2023-02-06 14:12:05 +0000

    product admin, securing views

--- profiles/views.py
+++ profiles/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
 
 from checkout.models import Order
 
 
+@login_required
 def profile(request):
-    """ Display the user's profile. """
+    """ Display the user's profile. """    
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':

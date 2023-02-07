65cf5e5a8498c2fa6359206b69c2fa2e6b2cdf40
Michelle Hickey <michellehickey2@yahoo.ie>
2023-02-07 20:22:19 +0000

    deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn essenchelle.wsgi:application
--- essenchelle/settings.py
+++ essenchelle/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
+import dj_database_url
 
 from pathlib import Path
 import os
@@ -28,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['essenchelle-oils.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -124,12 +126,17 @@ WSGI_APPLICATION = 'essenchelle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation

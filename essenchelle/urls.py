b6c467b70d87bb25905ec60b44f3846eb68b8844
Michelle Hickey <michellehickey2@yahoo.ie>
2023-02-12 15:47:28 +0000

    added 404 page for project

--- essenchelle/urls.py
+++ essenchelle/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +29,6 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'boutique_ado.views.handler404'
+

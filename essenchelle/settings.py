245c858e3b9925b25c919e73a06b907a7e8cd8da
Michelle Hickey <michellehickey2@yahoo.ie>
2023-03-06 17:45:00 +0000

    working on issue with checkout page

--- essenchelle/settings.py
+++ essenchelle/settings.py
@@ -30,6 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
+# DEBUG = False
+
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['essenchelle-oils.herokuapp.com', 'localhost']

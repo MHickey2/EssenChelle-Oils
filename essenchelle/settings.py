f1e3b358947afa52f3bbbd98bf32649fa6fcacb4
Michelle Hickey <michellehickey2@yahoo.ie>
2023-03-16 16:48:34 +0000

    trying to fix issue with emails and testing for webhooks

--- essenchelle/settings.py
+++ essenchelle/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
 
 DEBUG = False
 
@@ -266,4 +266,3 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-
